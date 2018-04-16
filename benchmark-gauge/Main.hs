485a66b01b7cecb42f55cb785cd3a34baa91d7e0
Andrew Martin <andrew.thaddeus@gmail.com>
2018-04-16 14:54:41 -0400

    add benchmarks to ensure that quickselect is O(n)

--- benchmark-gauge/Main.hs
+++ benchmark-gauge/Main.hs
@@ -3,6 +3,24 @@ import Gauge.Main
 import System.Random (randoms,mkStdGen)
 import qualified Data.Primitive.PrimArray.Median as PAM
 
+main :: IO ()
+main = defaultMain
+  [ bgroup "naive"
+    [ bench "sorted" $ whnf naiveIntegralMedian sortedList
+    , bench "random" $ whnf naiveIntegralMedian randomList
+    ]
+  , bgroup "primitive"
+    [ bench "sorted" $ whnf PAM.word sortedArray
+    , bench "random" $ whnf PAM.word randomArray
+    , bgroup "sized"
+      [ bench "1000" $ whnf PAM.word randomArray3
+      , bench "10000" $ whnf PAM.word randomArray4
+      , bench "100000" $ whnf PAM.word randomArray5
+      , bench "1000000" $ whnf PAM.word randomArray6
+      ]
+    ]
+  ]
+
 elemCount :: Int
 elemCount = 50000
 
@@ -13,19 +31,19 @@ sortedArray :: PrimArray Word
 sortedArray = fromList sortedList
 
 randomList :: [Word]
-randomList = randoms (mkStgGen 42)
+randomList = take elemCount (randoms (mkStgGen 42))
 
 randomArray :: PrimArray Word
 randomArray = fromList randomList
 
-main :: IO ()
-main = defaultMain
-  [ bgroup "sorted"
-    [ bench "naive" $ whnf naiveIntegralMedian sortedList
-    , bench "primitive" $ whnf PAM.word sortedArray
-    ]
-  , bgroup "random"
-    [ bench "naive" $ whnf naiveIntegralMedian randomList
-    , bench "primitive" $ whnf PAM.word randomArray
-    ]
-  ]
+randomArray3 :: PrimArray Word
+randomArray3 = fromList (take 1000 (randoms (mkStdGen 75843)))
+
+randomArray4 :: PrimArray Word
+randomArray4 = fromList (take 10000 (randoms (mkStdGen 75843)))
+
+randomArray5 :: PrimArray Word
+randomArray5 = fromList (take 100000 (randoms (mkStdGen 75843)))
+
+randomArray6 :: PrimArray Word
+randomArray6 = fromList (take 1000000 (randoms (mkStdGen 75843)))
