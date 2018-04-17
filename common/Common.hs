f1eed319a493da795fd61940e21bf646cd457a37
chessai <chessai1996@gmail.com>
2018-04-17 11:18:12 -0400

    make test suite build

--- common/Common.hs
+++ common/Common.hs
@@ -2,13 +2,14 @@ module Common
   ( naiveIntegralMedian
   ) where
 
-naiveIntegralMedian :: (Ord a, Num a) => [a] -> a
+import qualified Data.List as L
+import qualified Data.Map as M
+
+naiveIntegralMedian :: (Integral a, Ord a, Num a) => [a] -> a
 naiveIntegralMedian xs
   | sz < 1 = 0
   | odd sz = sortedList !! (div sz 2)
   | otherwise = div ((sortedList !! (div sz 2)) + (sortedList !! (div sz 2 + 1))) 2
   where
   sz = L.length xs
-  m = foldr (\x m -> M.insertWith (+) x 1) M.empty xs
-  sortedList = M.foldrWithKey (\x ct xs -> replicate ct x ++ xs) [] m
-
+  sortedList = L.sort xs
--- test/Main.hs
+++ test/Main.hs
@@ -1,3 +1,8 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RankNTypes #-}
+
+module Main (main) where
+
 import Test.Tasty
 import Test.Tasty.QuickCheck as QC
 
@@ -6,20 +11,27 @@ import Data.Ord
 import qualified Data.Map.Strict as M
 import qualified GHC.OldList as L
 
+import qualified Data.Primitive.PrimArray.Median as PAM
+import Control.Monad.ST
+import Data.Primitive.PrimArray
+import Data.Primitive.Types (Prim)
+
 import Common (naiveIntegralMedian)
+
+main :: IO ()
+main = defaultMain tests
 
 tests :: TestTree
 tests = testGroup "Median"
   [ QC.testProperty "int8"
-      $ \xs -> naiveMedian xs === fastMedian PAM.int8
+      $ \xs -> naiveIntegralMedian xs === fastMedian PAM.int8 xs
   , QC.testProperty "word8"
-      $ \xs -> naiveMedian xs === fastMedian PAM.word8
+      $ \xs -> naiveIntegralMedian xs === fastMedian PAM.word8 xs
   ]
 
-fastMedian :: (forall s. MutablePrimArray s a -> ST s a) -> [a] -> a
+fastMedian :: (Prim a) => (forall s. MutablePrimArray s a -> ST s a) -> [a] -> a
 fastMedian f xs = runST $ do
   let arr = fromList xs
   marr <- newPrimArray (sizeofPrimArray arr)
   copyPrimArray marr 0 arr 0 (sizeofPrimArray arr)
   f marr
-
